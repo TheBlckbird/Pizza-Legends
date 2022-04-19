65a97d1f5d28bd6516b0ca02d0a0985f1a80dc22
1Spinne <account@louisweigel.com>
2022-04-19 10:39:18 +0200

    add draw method to Sprite()

--- Sprte.js
+++ Sprte.js
@@ -17,4 +17,17 @@ class Sprite {
         this.currentAnimation = config.curentAnimation || "idleDown"
         this.currentAnimationFrame = 0
     }
+
+    draw(ctx) {
+        const x = this.gameObject.x * 16 - 8
+        const y = this.gameObject.y * 16 - 18
+
+        ctx.drawImage(
+            this.image,
+            0, 0,
+            32, 32,
+            x, y,
+            32, 32
+        )
+    }
 }
