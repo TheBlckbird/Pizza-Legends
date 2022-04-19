07a3a3cdc38bc5656082c2b4c6b3b51e7208b6c9
1Spinne <account@louisweigel.com>
2022-04-19 11:00:22 +0200

    add timeout

--- Overworld.js
+++ Overworld.js
@@ -26,8 +26,10 @@ class Overworld {
             src: "/images/characters/people/npc1.png"
         })
 
-        hero.sprite.draw(this.ctx)
-        npc1.sprite.draw(this.ctx)
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx)
+            npc1.sprite.draw(this.ctx)
+        }, 200)
 
     }
 
