cd8df38ab6d63bedcf31b75268c51c30a95c42f5
1Spinne <account@louisweigel.com>
2022-04-19 13:33:40 +0200

    add keys eventlistener

--- DirectionInput.js
+++ DirectionInput.js
@@ -1,6 +1,6 @@
 class DirectionInput {
     constructor() {
-        this.heldDirection = []
+        this.heldDirections = []
 
         this.map = {
             "ArrowUp": "up",
@@ -16,7 +16,17 @@ class DirectionInput {
 
     init() {
         document.addEventListener("keydown", e => {
-            console.log(e.code)
+            const dir = this.map[e.code]
+            if (dir && this.heldDirections.indexOf(dir) === -1) {
+                this.heldDirections.unshift(dir)
+            }
+        })
+        document.addEventListener("keyup", e => {
+            const dir = this.map[e.code]
+            const index = this.heldDirections.indexOf(dir)
+            if (index > -1) {
+                this.heldDirections.splice(index, 1)
+            }
         })
     }
 }
