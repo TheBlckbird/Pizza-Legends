d833315b9891bee7c20cbe8cc864dbcece71739d
1Spinne <account@louisweigel.com>
2022-04-19 16:53:59 +0200

    mcuh animation stuff

--- Sprite.js
+++ Sprite.js
@@ -20,10 +20,16 @@ class Sprite {
 
         // Configure Animation & Initial State
         this.animations = config.animations || {
-            "idle-down": [ [0,0] ],
-            "walk-down": [ [1,0], [0,0], [3,0], [0,0] ],
+            "idle-down" : [ [0,0] ],
+            "idle-up"   : [ [0,1] ],
+            "idle-left" : [ [0,2] ],
+            "idle-right": [ [0,3] ],
+            "walk-down" : [ [1,0], [0,0], [3,0], [0,0] ],
+            "walk-up"   : [ [1,1], [0,1], [3,1], [0,1] ],
+            "walk-left" : [ [1,2], [0,2], [3,2], [0,2] ],
+            "walk-right": [ [1,3], [0,3], [3,3], [0,3] ]
         }
-        this.currentAnimation = config.curentAnimation || "idle-down"
+        this.currentAnimation = "walk-down"// config.curentAnimation || "idle-down"
         this.currentAnimationFrame = 0
 
         this.animationFrameLimit = 16
@@ -34,18 +40,50 @@ class Sprite {
 
     }
 
+    get frame() {
+        return this.animations[this.currentAnimation][this.currentAnimationFrame]
+    }
+
+    setAnimation(key) {
+        if (this.currentAnimation !== key) {
+            this.currentAnimation = key
+            this.currentAnimationFrame = 0
+            this.animationFrameProgress = this.animationFrameLimit
+        }
+    }
+
+    updateAnimationProgress() {
+        // Downtick frame progress
+        if (this.animationFrameProgress > 0) {
+            this.animationFrameProgress -= 1
+            return
+        }
+
+        // Reset the counter
+        this.animationFrameProgress = this.animationFrameLimit
+        this.currentAnimationFrame += 1
+
+        if (this.frame === undefined) {
+            this.currentAnimationFrame = 0
+        }
+    }
+
     draw(ctx) {
         const x = this.gameObject.x - 8
         const y = this.gameObject.y - 18
 
         this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
 
+        const [frameX, frameY] = this.frame
+
         ctx.drawImage(
             this.image,
-            0, 0,
+            frameX * 32, frameY * 32,
             32, 32,
             x, y,
             32, 32
         )
+
+        this.updateAnimationProgress()
     }
 }
