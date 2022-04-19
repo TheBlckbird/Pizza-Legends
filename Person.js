dc291137a827adcb560b0b3499876c1ae80e57c7
1Spinne <account@louisweigel.com>
2022-04-19 12:46:21 +0200

    add basic movemnt vars

--- Person.js
+++ Person.js
@@ -2,5 +2,22 @@ class Person extends GameObject {
     constructor(config) {
         super(config)
         this.movingProgressRemaining = 0
+
+        this.directionUpdate = {
+            "down": ["y", 1],
+            "up": ["y", -1],
+            "left": ["x", -1],
+            "right": ["x", 1]
+        }
+    }
+
+    
+
+    updatePosition() {
+        if (this.movingProgressRemaining > 0) {
+            const [property, change] = this.directionUpdate[this.direction]
+            this[property] += change
+            this.movingProgressRemaining -= 1
+        }
     }
 }
