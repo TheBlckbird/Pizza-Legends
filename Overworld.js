f28e855a213f7bc620c69bbb0fb9579ae860b671
1Spinne <account@louisweigel.com>
2022-04-19 12:11:52 +0200

    canvas clearing

--- Overworld.js
+++ Overworld.js
@@ -9,6 +9,9 @@ class Overworld {
     startGameLoop() {
         const step = () => {
 
+            // Clear the canvas
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+            
             // Draw lowerImage
             this.map.drawLowerImage(this.ctx)
 
