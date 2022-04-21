0c236a8def3191884c3d304f82c9524b213f308b
1Spinne <account@louisweigel.com>
2022-04-21 20:52:14 +0200

    add method skelett and initialize OverworldEvent

---
+++ OverworldEvent.js
@@ -0,0 +1,18 @@
+class OverworldEvent {
+    constructor({ map, event }) {
+        this.map = map
+        this.event = event
+    }
+
+    stand() {
+
+    }
+
+    walk() {
+
+    }
+
+    init() {
+        
+    }
+}
