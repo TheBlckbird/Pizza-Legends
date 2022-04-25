65f44eedc7b7116f4b93b943d1cfc261acc8ba6c
Louis Weigel <account@louisweigel.com>
2022-04-25 17:45:30 +0200

    dismiss a text message

--- TextMessage.js
+++ TextMessage.js
@@ -13,6 +13,16 @@ class TextMessage {
             <p class="TextMessage_p">${this.text}</p>
             <button class="TextMessage_button">Next</button>
         `)
+
+        this.element.querySelector("button").addEventListener("click", () => {
+            this.done()
+        })
+
+    }
+
+    done() {
+        this.element.remove()
+        this.onComplete()
     }
 
     init(container) {
