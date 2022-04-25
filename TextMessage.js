b70c880ac095a603e523e68d910bb3bc3a956e88
Louis Weigel <account@louisweigel.com>
2022-04-25 16:19:56 +0200

    add createElement content

--- TextMessage.js
+++ TextMessage.js
@@ -6,7 +6,13 @@ class TextMessage {
     }
 
     createElement() {
-        // Create the element
+        this.element = document.createElement("div")
+        this.element.classList.add("TextMessage")
+
+        this.element.innerHTML = (`
+            <p class="TextMessage_p">${this.text}</p>
+            <button class="TextMessage_button">Next</button>
+        `)
     }
 
     init(container) {
